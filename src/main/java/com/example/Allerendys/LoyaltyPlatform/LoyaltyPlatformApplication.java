295cb1a0af463092bc5dba3b039a834655c82acd
Matteo Mancinelli <matteo.mancinelli222@gmail.com>
2023-01-23 17:56:45 +0100

    Primo Commit

---
+++ src/main/java/com/example/Allerendys/LoyaltyPlatform/LoyaltyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.Allerendys.LoyaltyPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Allerendys/LoyaltyPlatform/LoyaltyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Allerendys.LoyaltyPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
