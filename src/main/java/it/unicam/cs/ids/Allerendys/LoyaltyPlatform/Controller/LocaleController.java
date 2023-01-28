db4f6de9b253ef7462c9a511c4be55a6848e88cb
Matteo Mancinelli <matteo.mancinelli222@gmail.com>
2023-01-28 19:14:58 +0100

    fixing

--- src/main/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/Controller/LocaleController.java
+++ src/main/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/Controller/LocaleController.java
@@ -1,9 +1,14 @@
 package it.unicam.cs.ids.Allerendys.LoyaltyPlatform.Controller;
 
+import it.unicam.cs.ids.Allerendys.LoyaltyPlatform.Model.Locale;
 import it.unicam.cs.ids.Allerendys.LoyaltyPlatform.Service.LocaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/locale")
@@ -12,5 +17,14 @@ public class LocaleController {
     public LocaleService localeService;
 
 
+    @GetMapping("/all")
+    public List<Locale> getLocali(){
+        return localeService.getLocali();
+    }
+
+
+    public Optional<Locale> getLocale(String id){
+        return localeService.getLocale(id);
+    }
 
 }
