60b93b3efced7c60ad50ef27615fcde6da5f1649
Matteo Mancinelli <matteo.mancinelli222@gmail.com>
2023-01-28 14:00:21 +0100

    Fixinng path

--- src/main/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/LoyaltyPlatformApplication.java
+++ src/main/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/LoyaltyPlatformApplication.java
@@ -1,8 +1,10 @@
-package com.example.Allerendys.LoyaltyPlatform;
+package it.unicam.cs.ids.Allerendys.LoyaltyPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LoyaltyPlatformApplication {
 
--- src/test/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/LoyaltyPlatformApplicationTests.java
+++ src/test/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/LoyaltyPlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Allerendys.LoyaltyPlatform;
+package it.unicam.cs.ids.Allerendys.LoyaltyPlatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
