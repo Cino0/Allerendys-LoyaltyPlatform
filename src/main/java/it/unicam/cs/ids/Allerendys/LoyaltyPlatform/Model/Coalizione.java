5e7423bd994a642f169a73cc10bbf65d15b1ddf6
Innominato42 <enrico.privitera@gmail.com>
2023-02-20 18:27:16 +0100

    coalizione

---
+++ src/main/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/Model/Coalizione.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.ids.Allerendys.LoyaltyPlatform.Model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+
+public class Coalizione {
+
+    @Id
+    private String id;
+
+    private String nome;
+
+    private Programma programma;
+
+    private List<Locale> locali;
+}
---
+++ src/main/java/it/unicam/cs/ids/Allerendys/LoyaltyPlatform/Repository/CoalizioneRepository.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.ids.Allerendys.LoyaltyPlatform.Repository;
+
+import it.unicam.cs.ids.Allerendys.LoyaltyPlatform.Model.Coalizione;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoalizioneRepository extends MongoRepository<Coalizione,String> {
+}
